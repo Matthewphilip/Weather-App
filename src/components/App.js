f2873274fce9c6461d0f39ecbecddd4daf684e2a
Matthew Wiggins <mattwiggins5@gmail.com>
2022-07-01 15:09:42 +0100

    forecast summaries added

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,36 @@ import "../styles/App.css";
 import React from "react";
 import PropTypes from "prop-types";
 import LocationDetails from "./LocationDetails";
+import ForecastSummaries from "./ForecastSummaries";
 
-function App({ location }) {
-  const { city, country } = location;
-  return <LocationDetails city={city} country={country} />;
-}
+function App({location, forecasts}) {
+  return (
+    <div className="forecast">
+      <LocationDetails 
+        city={location.city} 
+        country={location.country} 
+      />
+      <ForecastSummaries forecasts={forecasts} />
+    </div>
+  );
+};
 
 App.propTypes = {
+  forecasts: PropTypes.arrayOf(
+    PropTypes.shape({
+      date: PropTypes.number,
+      description: PropTypes.string,
+      icon: PropTypes.string,
+      temperature: PropTypes.shape({
+        max: PropTypes.number,
+        min: PropTypes.number,
+      }),
+    })
+  ).isRequired,
   location: PropTypes.shape({
     city: PropTypes.string,
     country: PropTypes.string,
   }).isRequired,
 };
+
 export default App;
