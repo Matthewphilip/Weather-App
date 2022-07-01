91270ebfb49248565ced6902029a85a713771fd6
Matthew Wiggins <mattwiggins5@gmail.com>
2022-07-01 15:59:27 +0100

    initial css styles added

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,17 @@
-import "../styles/App.css";
 import React from "react";
 import PropTypes from "prop-types";
 import LocationDetails from "./LocationDetails";
 import ForecastSummaries from "./ForecastSummaries";
+import "../styles/App.css";
 
-function App({location, forecasts}) {
+function App({ location, forecasts }) {
   return (
-    <div className="forecast">
-      <LocationDetails 
-        city={location.city} 
-        country={location.country} 
-      />
+    <div className="weather-app">
+      <LocationDetails city={location.city} country={location.country} />
       <ForecastSummaries forecasts={forecasts} />
     </div>
   );
-};
+}
 
 App.propTypes = {
   forecasts: PropTypes.arrayOf(
